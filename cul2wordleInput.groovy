7e591e78c6e49d87599b8480719cb6a3b4f59e15
Egon Willighagen <egon.willighagen@gmail.com>
2010-10-31 00:35:36 +0200

    Added script to extract titles to act as input to Wordle

---
+++ cul2wordleInput.groovy
@@ -0,0 +1,43 @@
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
+import static groovyx.net.http.ContentType.JSON
+
+culUrl = "http://www.citeulike.org/";
+
+citotags = [
+  "cito--cites",
+  "cito--usesMethodIn",
+  "cito--discusses",
+  "cito--extends" // there are more, but these are all I use right now
+]
+
+papers = [
+  "1073448",
+  "423382"
+]
+
+http = new HTTPBuilder(culUrl)
+
+papers.each { paper ->
+  println "# Processing $paper..."
+  citotags.each { tag ->
+    citation = "$tag--$paper".toLowerCase()
+    http.request(Method.valueOf("GET"), JSON) {
+      uri.path = "/json/user/egonw/tag/$citation"
+
+      response.success = { resp,json ->
+        json.each { article ->
+          tripleCount = 0;
+          article.tags.each { artTag ->
+            if (artTag.startsWith(tag)) tripleCount++
+          }
+          if (tripleCount > 0) {
+            title = article.title
+            title = title.replaceAll("\\{","").replaceAll("\\}","")
+            println "$title"
+          }
+        }
+      }
+    }
+  }
+}
