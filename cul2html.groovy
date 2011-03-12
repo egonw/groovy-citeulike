890e942aef7bdeba6a8f707ef5e70f51044f54f0
Egon Willighagen <egon.willighagen@gmail.com>
2011-03-12 20:21:28 +0100

    Use a cached JSON version; output year + authors

--- cul2html.groovy
+++ cul2html.groovy
@@ -3,37 +3,59 @@ import groovyx.net.http.Method
 import groovy.xml.MarkupBuilder
 import static groovyx.net.http.ContentType.JSON
 
-culUrl = "http://www.citeulike.org/";
+// culUrl = "http://www.citeulike.org/";
+culUrl = "file:///home/egonw/";
 culAccount = "egonw";
 
 http = new HTTPBuilder(culUrl)
 
+def outputAuthors = { xml, authors ->
+  xml.span{
+    authors.each { author ->
+      xml.span(author) + ', ' 
+    }
+  }
+}
+
 def writer = new StringWriter()
 def xml = new MarkupBuilder(writer)
+
 xml.'html'(
   'xmlns':'http://www.w3.org/1999/xhtml',
-  'xmlns:bibo':'http://purl.org/ontology/bibo/'
+  'xmlns:bibo':'http://purl.org/ontology/bibo/',
+  'xmlns:dcterms':'http://purl.org/dc/terms/'
 ) {
   http.request(Method.valueOf("GET"), JSON) {
-    uri.path = "/json/user/$culAccount/publications"
+    // uri.path = "/json/user/$culAccount/publications"
+    uri.path = "/publications"
 
     response.success = { resp,json ->
-      xml.body() {
+      xml.body() { xml.ol() {
         json.each { article ->
-          xml.div('id':'#'+article.article_id, typeof:'bibo:Article') {
-            // this is where I should use CSL
-            title = article.title
-            title = title.replaceAll("\\{","").replaceAll("\\}","")
-            if (article.type.equals("BOOK")) {
-              xml.span(property:'bibo:title', article.title)
-            } else if (article.type.equals("JOUR")) {
-              xml.span(property:'bibo:title', article.title)
-            } else { // MISC
-              xml.span(property:'bibo:title', article.title)
+          // this is where I should use CSL
+          title = article.title
+          title = title.replaceAll("\\{","").replaceAll("\\}","")
+          if (article.type.equals("BOOK")) {
+            xml.li('id':'#'+article.article_id, typeof:'bibo:Article') {
+              outputAuthors(xml, article.authors)
+              xml.span(property:'bibo:title', article.title) + ', '
+              xml.span(property:'dcterms:created', article.year)
+            }
+          } else if (article.type.equals("JOUR")) {
+            xml.li('id':'#'+article.article_id, typeof:'bibo:Book') {
+              outputAuthors(xml, article.authors)
+              xml.span(property:'bibo:title', article.title) + ', '
+              xml.span(property:'dcterms:created', article.year)
+            }
+          } else { // MISC
+            xml.li('id':'#'+article.article_id, typeof:'bibo:Document') {
+              outputAuthors(xml, article.authors)
+              xml.span(property:'bibo:title', article.title) + ', '
+              xml.span(property:'dcterms:created', article.year)
             }
           }
         }
-      }
+      }}
     }
   }
 }
